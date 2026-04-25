7e6e85494de839d0e9a460f51832f0e0cd56cdea
Will <willauunsw@gmail.com>
2026-04-25 19:38:23 +1000

    update the deploy url

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000' 
-    : '${window.CONFIG.API_BASE_URL}';
+    : 'https://repair-store-web01.onrender.com';
 
 window.CONFIG = {
     API_BASE_URL
